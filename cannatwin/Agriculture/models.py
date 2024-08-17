c7f7a46039f91a9de7e978d1586822073c45254b
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2024-08-17 10:47:34 +0530

    Update models.py

--- cannatwin/Agriculture/models.py
+++ cannatwin/Agriculture/models.py
@@ -1,8 +1,1 @@
 
-# models.py
-from django.db import models
-
-class HarvestedData(models.Model):
-    file_name = models.CharField(max_length=255)
-    file_content = models.JSONField()  # Store file content as JSON or other relevant format
-    upload_date = models.DateTimeField(auto_now_add=True)
