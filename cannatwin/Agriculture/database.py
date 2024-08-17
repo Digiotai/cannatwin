f3909d26a30c3aeabe50b708802cc644773ccc3b
Rammohan_Rangam <96642590+RamuRamu-12@users.noreply.github.com>
2024-08-17 12:32:01 +0530

    Update database.py

--- cannatwin/Agriculture/database.py
+++ cannatwin/Agriculture/database.py
@@ -30,38 +30,50 @@
 #             if conn is not None:
 #                 cursor = conn.cursor()
                 
-#                 # Create user_details table with user_id as the primary key
+               
+                
+#                 # Create user_details table with user_id as the primary key and email as unique
 #                 user_details_query = """
 #                 CREATE TABLE IF NOT EXISTS user_details (
-#                   user_id SERIAL PRIMARY KEY,
-#                   user_name VARCHAR(40) NOT NULL, 
+#                   user_name VARCHAR(40) NOT NULL,
 #                   password VARCHAR(40) NOT NULL,
 #                   first_name VARCHAR(40) NOT NULL,
 #                   last_name VARCHAR(40) NOT NULL,
 #                   address VARCHAR(150) NOT NULL,
-#                   email VARCHAR(50),
+#                   email VARCHAR(50) UNIQUE NOT NULL,
 #                   mobile VARCHAR(14)
 #                 );
 #                 """
 #                 cursor.execute(user_details_query)
                 
-#                 # Create user_uploaded_data table with a foreign key reference to user_id
+#                 # Create user_uploaded_data table with a foreign key reference to email
 #                 user_uploaded_data_query = """
 #                 CREATE TABLE IF NOT EXISTS user_uploaded_data (
 #                   id SERIAL PRIMARY KEY,
-#                   user_id INTEGER NOT NULL REFERENCES user_details(user_id),
+#                   email VARCHAR(50) NOT NULL REFERENCES user_details(email),
 #                   data JSONB,
 #                   uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 #                 );
 #                 """
 #                 cursor.execute(user_uploaded_data_query)
 
+#                 # Create user_uploaded_data table with a foreign key reference to email
+#                 user_uploaded_data_for_harvest_query = """
+#                 CREATE TABLE IF NOT EXISTS user_uploaded_data_for_harvest (
+#                   id SERIAL PRIMARY KEY,
+#                   email VARCHAR(50) NOT NULL REFERENCES user_details(email),
+#                   data JSONB,
+#                   uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+#                 );
+#                 """
+#                 cursor.execute(user_uploaded_data_for_harvest_query)
+
+
 #                 conn.commit()
 #                 cursor.close()
 #                 conn.close()
 #         except Exception as e:
-#   
-#           print(e)
+#             print(e)
 
 #     def table_deletion(self, table_name):
 #         try:
@@ -75,6 +87,7 @@
 #                 conn.close()
 #         except Exception as e:
 #             print(e)
+
 
 #     def add_user(self, user_name, password, first_name, last_name, address, email, mobile):
 #         try:
@@ -94,13 +107,13 @@
 #         except Exception as e:
 #             print(e)
 
-#     def get_user_data(self, user_name):
+#     def get_user_data(self, email):
 #         try:
 #             conn = self.connect()
 #             if conn is not None:
 #                 cursor = conn.cursor()
-#                 query = "SELECT * FROM user_details WHERE user_name=%s;"
-#                 cursor.execute(query, (user_name,))
+#                 query = "SELECT * FROM user_details WHERE email=%s;"
+#                 cursor.execute(query, (email,))
 #                 res = cursor.fetchone()
 #                 cursor.close()
 #                 conn.close()
@@ -108,7 +121,7 @@
 #         except Exception as e:
 #             print(e)
 
-#     def store_file_data(self, user_id, df):
+#     def store_file_data(self, email, df):
 #         try:
 #             conn = self.connect()
 #             if conn is not None:
@@ -116,40 +129,103 @@
 #                 # Convert DataFrame to JSON format
 #                 data_json = df.to_json(orient='records')
 #                 query = """
-#                 INSERT INTO user_uploaded_data (user_id, data) 
+#                 INSERT INTO user_uploaded_data (email, data) 
 #                 VALUES (%s, %s);
 #                 """
-#                 cursor.execute(query, (user_id, data_json))
+#                 cursor.execute(query, (email, data_json))
 #                 conn.commit()
 #                 cursor.close()
 #                 conn.close()
 #         except Exception as e:
 #             print(e)
 
-#     def get_uploaded_data(self, user_id):
+
+#     def store_file_data_for_harvest(self, email, df):
 #         try:
 #             conn = self.connect()
 #             if conn is not None:
 #                 cursor = conn.cursor()
+#                 # Convert DataFrame to JSON format
+#                 data_json = df.to_json(orient='records')
 #                 query = """
-#                 SELECT data FROM user_uploaded_data 
-#                 WHERE user_id = %s;
+#                 INSERT INTO user_uploaded_data_for_harvest (email, data) 
+#                 VALUES (%s, %s);
 #                 """
-#                 cursor.execute(query, (user_id,))
-#                 result = cursor.fetchall()
+#                 cursor.execute(query, (email, data_json))
+#                 conn.commit()
 #                 cursor.close()
 #                 conn.close()
-#                 # Convert the result from JSON format to a DataFrame
-#                 data = [pd.DataFrame(json.loads(row[0])) for row in result]
-#                 return data
 #         except Exception as e:
 #             print(e)
+
+#     def get_uploaded_data(self, email):
+#         try:
+#             conn = self.connect()
+#             if conn is not None:
+#               cursor = conn.cursor()
+#               query = """
+#               SELECT data FROM user_uploaded_data 
+#               WHERE email = %s;
+#               """
+#               cursor.execute(query, (email,))
+#               result = cursor.fetchall()
+#               cursor.close()
+#               conn.close()
+
+#             # If data is stored as JSON string, parse it; otherwise, just return it
+#             data = []
+#             for row in result:
+#                 if isinstance(row[0], str):
+#                     data.append(json.loads(row[0]))
+#                 else:
+#                     data.append(row[0])
+
+#             return data
+
+#         except Exception as e:
+#            print(e)
+#         return None
+    
+#     def get_uploaded_data_for_harvest(self, email):
+#         try:
+#             conn = self.connect()
+#             if conn is not None:
+#               cursor = conn.cursor()
+#               query = """
+#               SELECT data FROM user_uploaded_data_for_harvest 
+#               WHERE email = %s;
+#               """
+#               cursor.execute(query, (email,))
+#               result = cursor.fetchall()
+#               cursor.close()
+#               conn.close()
+
+#             # If data is stored as JSON string, parse it; otherwise, just return it
+#             data = []
+#             for row in result:
+#                 if isinstance(row[0], str):
+#                     data.append(json.loads(row[0]))
+#                 else:
+#                     data.append(row[0])
+
+#             return data
+
+#         except Exception as e:
+#            print(e)
+#         return None
+
 
 # if __name__ == "__main__":
 #     db = PostgreSQLDB(dbname='uibmogli', user='uibmogli', password='8ogImHfL_1G249lXtM3k2EAIWTRDH2mX')
 #     db.table_creation()
-#     user_id = db.add_user('test_user', 'password', 'First', 'Last', '123 Street', 'email@example.com', '1234567890')
-#     print(db.get_user_data('test_user'))
+#     #user_id = db.add_user('test_user5', 'password', 'First', 'Last', '123 Street', 'emaiwel@gmail.com', '1234567890')
+#     #print(db.get_user_data('test_user5'))
+
+    
+#    # print(db.get_user_data("test_user1"))
+
+
+
 
 
 
@@ -185,8 +261,6 @@ class PostgreSQLDB:
             if conn is not None:
                 cursor = conn.cursor()
                 
-               
-                
                 # Create user_details table with user_id as the primary key and email as unique
                 user_details_query = """
                 CREATE TABLE IF NOT EXISTS user_details (
@@ -212,6 +286,17 @@ class PostgreSQLDB:
                 """
                 cursor.execute(user_uploaded_data_query)
 
+                # Create user_uploaded_data table with a foreign key reference to email
+                user_uploaded_data_for_harvest_query = """
+                CREATE TABLE IF NOT EXISTS user_uploaded_data_for_harvest (
+                  id SERIAL PRIMARY KEY,
+                  email VARCHAR(50) NOT NULL REFERENCES user_details(email),
+                  data JSONB,
+                  uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );
+                """
+                cursor.execute(user_uploaded_data_for_harvest_query)
+
                 conn.commit()
                 cursor.close()
                 conn.close()
@@ -231,7 +316,6 @@ class PostgreSQLDB:
         except Exception as e:
             print(e)
 
-
     def add_user(self, user_name, password, first_name, last_name, address, email, mobile):
         try:
             conn = self.connect()
@@ -239,14 +323,12 @@ class PostgreSQLDB:
                 cursor = conn.cursor()
                 query = """
                 INSERT INTO user_details (user_name, password, first_name, last_name, address, email, mobile) 
-                VALUES(%s, %s, %s, %s, %s, %s, %s) RETURNING user_id;
+                VALUES(%s, %s, %s, %s, %s, %s, %s);
                 """
                 cursor.execute(query, (user_name, password, first_name, last_name, address, email, mobile))
-                user_id = cursor.fetchone()[0]  # Get the generated user_id
                 conn.commit()
                 cursor.close()
                 conn.close()
-                return user_id  # Return the user_id
         except Exception as e:
             print(e)
 
@@ -271,16 +353,70 @@ class PostgreSQLDB:
                 cursor = conn.cursor()
                 # Convert DataFrame to JSON format
                 data_json = df.to_json(orient='records')
-                query = """
-                INSERT INTO user_uploaded_data (email, data) 
-                VALUES (%s, %s);
-                """
-                cursor.execute(query, (email, data_json))
+
+                # Check if data already exists for the email
+                query_check = "SELECT id FROM user_uploaded_data WHERE email=%s;"
+                cursor.execute(query_check, (email,))
+                result = cursor.fetchone()
+
+                if result:
+                    # If data exists, update it
+                    query_update = """
+                    UPDATE user_uploaded_data 
+                    SET data = %s, uploaded_at = CURRENT_TIMESTAMP 
+                    WHERE email = %s;
+                    """
+                    cursor.execute(query_update, (data_json, email))
+                else:
+                    # If data does not exist, insert a new record
+                    query_insert = """
+                    INSERT INTO user_uploaded_data (email, data) 
+                    VALUES (%s, %s);
+                    """
+                    cursor.execute(query_insert, (email, data_json))
+
                 conn.commit()
                 cursor.close()
                 conn.close()
         except Exception as e:
             print(e)
+
+    def store_file_data_for_harvest(self, email, df):
+        try:
+            conn = self.connect()
+            if conn is not None:
+                cursor = conn.cursor()
+                # Convert DataFrame to JSON format
+                data_json = df.to_json(orient='records')
+
+                # Check if data already exists for the email
+                query_check = "SELECT id FROM user_uploaded_data_for_harvest WHERE email=%s;"
+                cursor.execute(query_check, (email,))
+                result = cursor.fetchone()
+
+                if result:
+                    # If data exists, update it
+                    query_update = """
+                    UPDATE user_uploaded_data_for_harvest 
+                    SET data = %s, uploaded_at = CURRENT_TIMESTAMP 
+                    WHERE email = %s;
+                    """
+                    cursor.execute(query_update, (data_json, email))
+                else:
+                    # If data does not exist, insert a new record
+                    query_insert = """
+                    INSERT INTO user_uploaded_data_for_harvest (email, data) 
+                    VALUES (%s, %s);
+                    """
+                    cursor.execute(query_insert, (email, data_json))
+
+                conn.commit()
+                cursor.close()
+                conn.close()
+        except Exception as e:
+            print(e)
+
+            
 
     def get_uploaded_data(self, email):
         try:
@@ -296,16 +432,28 @@ class PostgreSQLDB:
               cursor.close()
               conn.close()
 
-            # If data is stored as JSON string, parse it; otherwise, just return it
-            data = []
-            for row in result:
-                if isinstance(row[0], str):
-                    data.append(json.loads(row[0]))
-                else:
-                    data.append(row[0])
+              data = [json.loads(row[0]) if isinstance(row[0], str) else row[0] for row in result]
+              return data
+        except Exception as e:
+           print(e)
+        return None
+    
+    def get_uploaded_data_for_harvest(self, email):
+        try:
+            conn = self.connect()
+            if conn is not None:
+              cursor = conn.cursor()
+              query = """
+              SELECT data FROM user_uploaded_data_for_harvest 
+              WHERE email = %s;
+              """
+              cursor.execute(query, (email,))
+              result = cursor.fetchall()
+              cursor.close()
+              conn.close()
 
-            return data
-
+              data = [json.loads(row[0]) if isinstance(row[0], str) else row[0] for row in result]
+              return data
         except Exception as e:
            print(e)
         return None
@@ -314,9 +462,5 @@ class PostgreSQLDB:
 if __name__ == "__main__":
     db = PostgreSQLDB(dbname='uibmogli', user='uibmogli', password='8ogImHfL_1G249lXtM3k2EAIWTRDH2mX')
     db.table_creation()
-    #user_id = db.add_user('test_user5', 'password', 'First', 'Last', '123 Street', 'emaiwel@gmail.com', '1234567890')
-    #print(db.get_user_data('test_user5'))
-
-    
-    print(db.get_user_data("test_user1"))
+    # Add your test cases here
 
